90b633d5764d1a54990e59468a47f079d0430805
alex <raulalexo100@gmail.com>
2023-10-24 13:31:35 -0700

    Added multiple new schemas to database init scripts

--- database_scripts/mysql/00_schema.sql
+++ database_scripts/mysql/00_schema.sql
@@ -14,3 +14,71 @@ CREATE TABLE floreria.locales(
     numero INT NOT NULL,
     colonia VARCHAR(50) NOT NULL
 );
+
+CREATE SCHEMA tienda;
+
+CREATE TABLE tienda.productos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL
+);
+
+CREATE TABLE tienda.clientes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(50) NOT NULL
+);
+
+CREATE SCHEMA libreria;
+
+CREATE TABLE libreria.libros(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+);
+
+CREATE SCHEMA cafeteria;
+
+CREATE TABLE cafeteria.productos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE cafeteria.empleados(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    cargo VARCHAR(50) NOT NULL
+);
+
+CREATE SCHEMA gimnasio;
+
+CREATE TABLE gimnasio.clientes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_inicio DATE NOT NULL
+);
+
+CREATE TABLE gimnasio.equipos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    tipo VARCHAR(50) NOT NULL
+);
+
+CREATE SCHEMA hotel;
+
+CREATE TABLE hotel.habitaciones(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE hotel.reservas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    cliente_id INT NOT NULL
+);
--- database_scripts/postgres/00_schema.sql
+++ database_scripts/postgres/00_schema.sql
@@ -10,3 +10,64 @@ CREATE TABLE restaurante.vehiculos(
     modelo VARCHAR(50) NOT NULL,
     placas VARCHAR(10) NOT NULL
 );
+
+CREATE SCHEMA universidad;
+
+CREATE TABLE universidad.estudiantes(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    carrera VARCHAR(50) NOT NULL,
+    fecha_ingreso DATE NOT NULL
+);
+
+CREATE TABLE universidad.profesores(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    departamento VARCHAR(50) NOT NULL
+);
+
+CREATE SCHEMA banco;
+
+CREATE TABLE banco.clientes(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    saldo DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE banco.transacciones(
+    id SERIAL PRIMARY KEY,
+    fecha_hora TIMESTAMP NOT NULL,
+    cliente_id INT NOT NULL,
+    monto DECIMAL(10, 2) NOT NULL
+);
+
+CREATE SCHEMA teatro;
+
+CREATE TABLE teatro.obras(
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    fecha_estreno DATE NOT NULL
+);
+
+CREATE TABLE teatro.actores(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    edad INT NOT NULL
+);
+
+CREATE SCHEMA biblioteca;
+
+CREATE TABLE biblioteca.libros(
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    editorial VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE biblioteca.lectores(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL
+);
